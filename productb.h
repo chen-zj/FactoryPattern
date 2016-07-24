868ab6c5ea223c96402a33c286390e968811b1a0
chen-zj <1056327887@qq.com>
2016-07-24 10:58:09 +0800

    Create productb.h

---
+++ productb.h
@@ -0,0 +1,15 @@
+#ifndef PRODUCTB_H
+#define PRODUCTB_H
+
+#include "productb.h"
+
+class ProductB : public Product  
+{  
+public:  
+    ProductB();  
+  
+public:  
+    void operationB();  
+};  
+  
+#endif // PRODUCTB_H 
