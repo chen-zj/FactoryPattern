8a9b61d2517e82f206f7734ca1a8d746e98d997f
chen-zj <1056327887@qq.com>
2016-07-24 10:51:22 +0800

    Create product.h

---
+++ product.h
@@ -0,0 +1,13 @@
+#ifndef PRODUCT_H  
+#define PRODUCT_H  
+
+class Product  
+{  
+public:  
+   Product();  
+ 
+public:  
+   virtual void operationA();  
+   virtual void operationB();  
+};  
+#endif // PRODUCT_H  
